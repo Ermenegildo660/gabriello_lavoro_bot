7f57a50f8e6e30c7f60cd9fbe332cc219b24f7c9
Ermenegildo660 <gabryel.b00@gmail.com>
2025-11-18 16:55:17 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -258,15 +258,13 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # ----------------------------------------------------
 # MAIN
 # ----------------------------------------------------
-
-async def main():
+def main():
     app = ApplicationBuilder().token(os.getenv("BOT_TOKEN")).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
